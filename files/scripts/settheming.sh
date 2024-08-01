a7dc4a93967731c7f39cb9fb39f584bf7b78a446
EpixMan <abdullahgtgt@gmail.com>
2024-08-02 00:36:37 +0300

    migration to system (final)

--- files/scripts/settheming.sh
+++ files/scripts/settheming.sh
@@ -2,5 +2,5 @@
 
 # Tell build process to exit if there are any errors.
 set -oue pipefail
-
-sed -i 's/Inherits=Adwaita/Inherits=ePapirus/' /usr/share/icons/default/index.theme
+#qoijjj says it breaks the system
+#sed -i 's/Inherits=Adwaita/Inherits=ePapirus/' /usr/share/icons/default/index.theme
